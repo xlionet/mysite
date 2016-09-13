2b61bc3cad5b2313f591a27e5d7840715c539655
Passi <18855584909@163.com>
2016-09-13 18:12:07 +0800

    the post method test for user regist

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views 
 
+app_name = 'blog'
 urlpatterns = [
 		url(r'^$', views.index, name="index"),
+		url(r'^search/$', views.search, name='search'),
+		url(r'^result/$', views.result, name='result'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,26 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render_to_response
+from django.template import loader
 # Create your views here.
 
 def index( request ):
 		
 	return HttpResponse("Hello,This is my first site page!");
+
+#def UserLogin( request ):
+	
+def search( request ):
+		print "POST"
+		template = loader.get_template('blog/test.html')
+		context = {
+		}
+		return HttpResponse(template.render(context, request))
+		
+def result( request ):
+		print "POST"
+		username = request.POST['username']
+		password = request.POST['password']
+		print username
+		print password
+		return HttpResponse("i am result!");
