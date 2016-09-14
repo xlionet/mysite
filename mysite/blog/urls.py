521008f72688d166639d73789c0c22836a482344
Passi <18855584909@163.com>
2016-09-14 15:13:18 +0800

    regist,login,logout

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-		url(r'^$', views.index, name="index"),
-		url(r'^search/$', views.search, name='search'),
-		url(r'^result/$', views.result, name='result'),
+		url(r'^index/$', views.index, name="index"),
+		url(r'^regist/$', views.regist, name='regist'),
+		url(r'^doRegist/$', views.doRegist, name='doRegist'),
+		url(r'^login/$', views.userLogin, name='userlogin'),
+		url(r'^doLogin/$', views.doLogin, name='doLogin'),
+		url(r'^logout/$', views.logout, name='logout'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,26 +1,46 @@
 #from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
-from django.template import loader
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render_to_response,render
+from django.template import loader,RequestContext
+from django.contrib import auth
+from django.views import generic
+from django.contrib.auth.models import User
 # Create your views here.
 
 def index( request ):
 		
-	return HttpResponse("Hello,This is my first site page!");
+	return render(request, 'blog/index.html')
 
-#def UserLogin( request ):
-	
-def search( request ):
-		print "POST"
-		template = loader.get_template('blog/test.html')
+def userLogin( request ):
+		return render(request, 'blog/login.html')
+
+def doLogin( request ):
+		username = request.POST['username']
+		password = request.POST['password']
+		user = auth.authenticate(username=username, password=password)
+		if user and user.is_active:
+				auth.login(request, user)
+				print "login success!"
+				return HttpResponseRedirect("/blog/index")
+		return render_to_response("blog/index.html")
+		
+def regist( request ):
+		print "regist"
+		template = loader.get_template('blog/regist.html')
 		context = {
 		}
 		return HttpResponse(template.render(context, request))
 		
-def result( request ):
-		print "POST"
+def doRegist( request ):
+		print "doRegist"
 		username = request.POST['username']
+		usermail = request.POST['email']
 		password = request.POST['password']
-		print username
-		print password
-		return HttpResponse("i am result!");
+		user = User.objects.create_user(username,usermail,password)
+		user.save()
+		return HttpResponse("regist success!")
+		
+def logout( request ):
+		auth.logout(request)
+		print "userLogout"
+		return HttpResponseRedirect("/blog/index")
